661300c2070f2c3ea6105334a6293720ffab9538
Brian Ngoyoni <brianngoyoni254@gmail.com>
2026-03-24 17:40:03 +0300

    chore: updated grading.js and added difference.js

---
+++ difference.js
@@ -0,0 +1,12 @@
+//define a function
+function differencewith13(num) {
+    if (num > 13)//check if the number is greater than 13
+     {
+        return 2 * Math.abs(num - 13);
+    } else {
+        return Math.abs(num - 13);// give the difference and ensure the difference is always positive
+    }
+}
+//examples
+console.log(differencewith13(11));
+console.log(differencewith13(25));
--- grading.js
+++ grading.js
@@ -8,4 +8,18 @@ let grade;
 if( average<= 60){
     grade="F";
 }
-else if( average< =70)
+else if( average<=70){ 
+    grade="B";
+
+}
+else if( average <= 80){
+    grade="C";
+}
+else if(average <=90){
+    grade="B";
+}
+else {
+    grade ="A";
+}
+console.log("Average:"+ average);
+console.log("Grade:"+ grade);
